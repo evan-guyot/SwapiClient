680d2406e5812a96966dc998be42c2773604a7c0
Evan GUYOT <evan.guyot2022@campus-eni.fr>
2023-06-29 14:42:04 +0200

    feat:redesign-main-page

--- swapi-client/src/app/page.tsx
+++ swapi-client/src/app/page.tsx
@@ -41,6 +41,13 @@ export default function Home() {
       <Navbar />
       {swapiMenus ? (
         <main className="flex min-h-screen flex-col items-center p-24">
+          <div
+            aria-hidden="true"
+            className="absolute inset-0 grid grid-cols-2 -space-x-52 opacity-40 dark:opacity-20 -z-10"
+          >
+            <div className="blur-[106px] h-80 bg-gradient-to-br from-blue-400 to-purple-400 dark:from-blue-700"></div>
+            <div className="blur-[106px] h-64 bg-gradient-to-r from-purple-400 to-cyan-300 dark:to-indigo-600"></div>
+          </div>
           <SwapiMenus
             menus={swapiMenus}
             indexChanger={changeIndex}
@@ -51,10 +58,9 @@ export default function Home() {
       ) : (
         <main className="flex min-h-screen flex-col items-center bg-indigo-500 justify-center">
           <div className="flex flex-row animate-ping">
-            <p className="text-2xl font-semibold text-white">
-              Fetching your data
+            <p className="text-2xl font-semibold text-white mr-3">
+              Fetching your data...
             </p>
-            {"  "}
             <RocketLaunchIcon className="h-8 w-8 text-white" />
           </div>
         </main>
