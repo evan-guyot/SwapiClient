66c4b8c4f18341aab1156e89c0533170f5a5f55e
Evan GUYOT <evan.guyot2022@campus-eni.fr>
2023-07-03 16:56:08 +0200

    feat:creation-of-planets-component

--- swapi-client/src/interfaces/swapi.tsx
+++ swapi-client/src/interfaces/swapi.tsx
@@ -1,8 +1,11 @@
-//People
-export interface ISwapiPeople {
+interface ISwapiData {
   count: number;
   next: string | undefined;
   previous: string | undefined;
+}
+
+//People
+export interface ISwapiPeople extends ISwapiData {
   results: Array<ISwapiCharacter>;
 }
 
@@ -21,7 +24,17 @@ export interface ISwapiUser {
   gender: string;
 }
 //Planets
-export interface ISwapiPlanet {}
+export interface ISwapiPlanets extends ISwapiData {
+  results: Array<ISwapiPlanet>;
+}
+
+export interface ISwapiPlanet {
+  name: string;
+  climate: string;
+  gravity: string;
+  terrain: string;
+  population: string;
+}
 //Films
 export interface ISwapiFilm {}
 //Species
